f49dd8cca0ccbdd0c0d1be2c2b0c481f6ea56e18
KlykovVasiliy <Klykov_V@mail.ru>
2024-06-06 15:09:15 +0300

    display system information and a list of programs

--- InstalledPrograms/src/main/java/org/example/Main.java
+++ InstalledPrograms/src/main/java/org/example/Main.java
@@ -1,44 +1,19 @@
 package org.example;
 
 import org.example.config.ApplicationConfig;
-import org.example.model.Program;
-import org.example.services.ProgramService;
+import org.example.services.ComputerService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Comparator;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-//        var context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-//        var programService = context.getBean(ProgramService.class);
-//        programService.setPathRegistry(PathsInTheRegistry.PROGRAM_FILES);
-//        programService.setPathRegistry(PathsInTheRegistry.PROGRAM_FILES_X64);
-//
-//        programService.getListPrograms().stream()
-//                        .sorted(Comparator.comparing(Program::getDisplayName))
-//                                .forEach(System.out::println);
-//
-//        System.out.println(programService.getListPrograms().size());
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        var computerService = context.getBean(ComputerService.class);
 
-        Process process = Runtime.getRuntime().exec("systeminfo");
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                if (line.isEmpty()) {
-                    continue;
-                }
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+        System.out.println(computerService.getInformationAboutSystem());
+        System.out.println("\n");
+        computerService.getProgramList().forEach(System.out::println);
+        System.out.println("Количество установленных программ на рабочей станции: " +
+                computerService.getProgramList().size());
     }
 }
