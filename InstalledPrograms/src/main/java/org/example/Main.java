d732f4f6894813d8410b8e72988e28a8d8850f87
KlykovVasiliy <31406406+KlykovVasiliy@users.noreply.github.com>
2024-06-27 11:10:09 +0300

    write to Json file

--- InstalledPrograms/src/main/java/org/example/Main.java
+++ InstalledPrograms/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.example.config.ApplicationConfig;
 import org.example.services.ComputerService;
-import org.example.services.DocumentHtmlService;
+import org.example.writers.WriterDocument;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
@@ -11,9 +11,10 @@ public class Main {
         var context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         var computerService = context.getBean(ComputerService.class);
         var computer = computerService.getComputer();
-        var documentHtmlService = context.getBean(DocumentHtmlService.class);
 
-        documentHtmlService.setComputer(computer);
-        documentHtmlService.WriteToDocument();
+        var writeToHtml = context.getBean("writeToHtml", WriterDocument.class);
+        var writeToJson = context.getBean("writeToJson", WriterDocument.class);
+        writeToHtml.writerToDocument(computer);
+        writeToJson.writerToDocument(computer);
     }
 }
